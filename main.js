7cc7a6b2b70fa6e476902af2f28a0346a0c08969
Nahidul <nahisl12@gmail.com>
2020-04-01 21:16:07 +0100

    Added basic functionality to a few buttons and coded the display to update

--- main.js
+++ main.js
@@ -19,7 +19,7 @@ function operate(operator, a, b) {
         return add(a, b);
     }
     if(operator === '-') {
-        return subtract(a, b);
+        return subtract(a, b);  
     }
     if(operator === '*') {
         return multiply(a, b);
@@ -28,3 +28,32 @@ function operate(operator, a, b) {
         return divide(a, b);
     }
 }
+
+const display = document.querySelector('.display');
+const keys = document.querySelector('.keys');
+
+keys.addEventListener('click', function(event){
+    // if(event.target.matches('#zero')){
+    //     display.textContent = '1';
+    //     console.log('working');  
+    // }
+    const keyPressed = event.target;
+    const dataAction = keyPressed.dataset.action;
+    if(dataAction === 'zero') {
+        display.textContent = '0';
+        console.log('working');  
+    }
+    else if (dataAction == 'one'){
+        display.textContent = '1';
+        console.log('working'); 
+    }
+});
+
+
+// const keys = Array.from(document.querySelectorAll('.keys'));
+// keys.forEach(key => key.addEventListener('click', function(){
+//      if(key.matches('zero1')) {
+//         display.textContent = '1';
+//         console.log('working');  
+//      }
+// }));
