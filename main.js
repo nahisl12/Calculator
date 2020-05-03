b33a8d5c70568147086c85025a4aeed7ae9d4444
Nahidul <nahisl12@gmail.com>
2020-05-03 19:22:10 +0100

    added comments

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+/* Calculation Functions */
 function add(a, b) {
     return parseFloat(a) + parseFloat(b);
 }
@@ -14,7 +15,7 @@ function divide(a, b) {
     return parseFloat(a) / parseFloat(b);
 }
 
-function operate(operator, a, b) {
+function operate(operator, a, b) {  //Takes an operator and 2 variables to call/calculate the sum
     if(operator === '+') {
         return add(a, b);
     }
@@ -29,25 +30,27 @@ function operate(operator, a, b) {
     }
 }
 
+
+/*DOM Functions */
 const display = document.querySelector('.display');
 const keys = document.querySelector('.keys');
 let firstEntry;
 let secondEntry;
 let operation;
 
-keys.addEventListener('click', function(event){ 
-    const keyPressed = event.target;
-    const dataAction = keyPressed.dataset.action;
-    const keyContent = keyPressed.textContent;
-    let displayedNum = display.textContent;
+keys.addEventListener('click', function(event){     //Adds an eventlistener to the keys class to look for button clicks
+    const keyPressed = event.target;                //Checks for an event on whatever button is pressed
+    const dataAction = keyPressed.dataset.action;   //Checks for the data-action tag linked to the pressed button
+    const keyContent = keyPressed.textContent;      //Grabs whatever text/number is within the pressed key
+    let displayedNum = display.textContent;         //Holds whatever nums is currently displayed on screen
 
-        if(dataAction) {
-            if(displayedNum === '0') {
+        if(dataAction) {    //If a button is pressed
+            if(displayedNum === '0') {  //Check that a 0 is displayed and replace it with the number of the pressed button
                 display.textContent = keyContent;
-            } else {
+            } else {    //Otherwise run the statements below depending on what button is pressed
                 switch(dataAction) {
                     case 'zero':
-                        display.textContent = displayedNum + '0';
+                        display.textContent = displayedNum + '0';   //Concatanates the pressed num to the current display content
                         break;
                     case 'one':
                         display.textContent = displayedNum + '1';
@@ -80,8 +83,8 @@ keys.addEventListener('click', function(event){
                         display.textContent = displayedNum + '.';
                         break;  
                     case 'add':
-                        firstEntry = displayedNum;
-                        operation = '+';
+                        firstEntry = displayedNum;  //Stores the current displayed numbers into this function
+                        operation = '+';            //Sets the operation variable to value of the button that was pressed
                         display.textContent = '0';
                         break;
                     case 'subtract':
@@ -100,8 +103,8 @@ keys.addEventListener('click', function(event){
                         display.textContent = '0';
                         break;
                     case 'equal':
-                        secondEntry = displayedNum;
-                        display.textContent = operate(operation, firstEntry, secondEntry);
+                        secondEntry = displayedNum; //When equal is pressed the second set of entered nums are stored here
+                        display.textContent = operate(operation, firstEntry, secondEntry);  //Takes the 3 variables and calls the appropriate operator function
                         break;
                     case 'clear':
                         display.textContent = '0';
