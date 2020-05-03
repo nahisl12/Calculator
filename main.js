446890ac78ed752ba1cad7ec8ea15767a81a5d22
Nahidul <nahisl12@gmail.com>
2020-05-03 19:03:21 +0100

    Operate function now works when two numbers have been entered and equal is pressed

--- main.js
+++ main.js
@@ -1,17 +1,17 @@
 function add(a, b) {
-    return a + b;
+    return parseFloat(a) + parseFloat(b);
 }
 
 function subtract(a, b) {
-    return a - b;
+    return parseFloat(a) - parseFloat(b);
 }
 
 function multiply(a, b) {
-    return a * b;
+    return parseFloat(a) * parseFloat(b);
 }
 
 function divide(a, b) {
-    return a / b;
+    return parseFloat(a) / parseFloat(b);
 }
 
 function operate(operator, a, b) {
@@ -31,71 +31,82 @@ function operate(operator, a, b) {
 
 const display = document.querySelector('.display');
 const keys = document.querySelector('.keys');
+let firstEntry;
+let secondEntry;
+let operation;
 
-keys.addEventListener('click', function(event){
-    // if(event.target.matches('#zero')){
-    //     display.textContent = '1';
-    //     console.log('working');  
-    // }
-    let displayedNum = display.textContent;
-    let selectedNum;
+keys.addEventListener('click', function(event){ 
     const keyPressed = event.target;
     const dataAction = keyPressed.dataset.action;
-    // if(dataAction === 'zero') {
-    //     display.textContent = '0';
-    //     console.log('working');  
-    // }
-    // else if (dataAction == 'one'){
-    //     display.textContent = '1';
-    //     console.log('working'); 
-    // }
-        switch(dataAction) {
-            case 'zero':
-                display.textContent = displayedNum + '0';
-                break;
-            case 'one':
-                display.textContent = displayedNum + '1';
-                break;
-            case 'two':
-                display.textContent = displayedNum + '2';
-                break;
-            case 'three':
-                display.textContent = displayedNum + '3';
-                break;  
-            case 'four':
-                display.textContent = displayedNum + '4';
-                break;          
-            case 'five':
-                display.textContent = displayedNum + '5';
-                break;
-            case 'six':
-                display.textContent = displayedNum + '6';
-                break;
-            case 'seven':
-                display.textContent = displayedNum + '7';
-                break; 
-            case 'eight':
-                display.textContent = displayedNum + '8';
-                break;  
-            case 'nine':
-                display.textContent = displayedNum + '9';
-                break;  
-            case 'decimal':
-                display.textContent = displayedNum + '.';
-                break;  
-            case 'clear':
-                display.textContent = ' ';
-                break;
-            default:
-                break;
+    const keyContent = keyPressed.textContent;
+    let displayedNum = display.textContent;
+
+        if(dataAction) {
+            if(displayedNum === '0') {
+                display.textContent = keyContent;
+            } else {
+                switch(dataAction) {
+                    case 'zero':
+                        display.textContent = displayedNum + '0';
+                        break;
+                    case 'one':
+                        display.textContent = displayedNum + '1';
+                        break;
+                    case 'two':
+                        display.textContent = displayedNum + '2';
+                        break;
+                    case 'three':
+                        display.textContent = displayedNum + '3';
+                        break;  
+                    case 'four':
+                        display.textContent = displayedNum + '4';
+                        break;          
+                    case 'five':
+                        display.textContent = displayedNum + '5';
+                        break;
+                    case 'six':
+                        display.textContent = displayedNum + '6';
+                        break;
+                    case 'seven':
+                        display.textContent = displayedNum + '7';
+                        break; 
+                    case 'eight':
+                        display.textContent = displayedNum + '8';
+                        break;  
+                    case 'nine':
+                        display.textContent = displayedNum + '9';
+                        break;  
+                    case 'decimal':
+                        display.textContent = displayedNum + '.';
+                        break;  
+                    case 'add':
+                        firstEntry = displayedNum;
+                        operation = '+';
+                        display.textContent = '0';
+                        break;
+                    case 'subtract':
+                        firstEntry = displayedNum;
+                        operation = '-';
+                        display.textContent = '0';
+                        break;
+                    case 'multiply':
+                        firstEntry = displayedNum;
+                        operation = '*';
+                        display.textContent = '0';
+                        break;
+                    case 'divide':
+                        firstEntry = displayedNum;
+                        operation = '/';
+                        display.textContent = '0';
+                        break;
+                    case 'equal':
+                        secondEntry = displayedNum;
+                        display.textContent = operate(operation, firstEntry, secondEntry);
+                        break;
+                    case 'clear':
+                        display.textContent = '0';
+                        break;
+                }
+            }
         }
 });
-
-
-// const keys = Array.from(document.querySelectorAll('.keys'));
-// keys.forEach(key => key.addEventListener('click', function(){
-//      if(key.matches('zero1')) {
-//         display.textContent = '1';
-//         console.log('working');  
-//      }
-// }));
